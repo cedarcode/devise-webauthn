911017e87345c6a300706edf3354d96809698300
Renzo Minelli <renminelli@gmail.com>
2025-08-05 21:00:27 -0300

    add passkey authenticatable module using passkey model

---
+++ lib/devise/models/passkey_authenticatable.rb
@@ -0,0 +1,15 @@
+require "active_support/concern"
+
+module Devise
+  module Models
+    module PasskeyAuthenticatable
+      extend ActiveSupport::Concern
+
+      included do
+        has_many :passkeys, dependent: :destroy
+
+        validates :webauthn_id, uniqueness: true, allow_blank: true
+      end
+    end
+  end
+end
