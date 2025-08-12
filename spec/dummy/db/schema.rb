e3b6a66e8cbbdb9b7fbbf695cae690bfeeef4f5f
Renzo Minelli <renminelli@gmail.com>
2025-08-11 22:09:56 -0300

    remove unnecessary migration

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -18,9 +18,9 @@ ActiveRecord::Schema[8.0].define(version: 20_240_101_000_000) do
     t.string :encrypted_password, null: false, default: ""
     t.string :webauthn_id
     t.timestamps null: false
+    t.index :webauthn_id, unique: true
+    t.index :email, unique: true
   end
-  add_index :users, :email, unique: true
-  add_index :users, :webauthn_id, unique: true
 
   create_table :passkeys, force: true do |t|
     t.references :user, null: false
@@ -29,6 +29,6 @@ ActiveRecord::Schema[8.0].define(version: 20_240_101_000_000) do
     t.string :name, null: false
     t.integer :sign_count, null: false, default: 0
     t.timestamps null: false
+    t.index :external_id, unique: true
   end
-  add_index :passkeys, :external_id, unique: true
 end
