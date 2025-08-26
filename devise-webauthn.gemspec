241528f6a21b36e8af60df1a6cac8b0b189d8f9f
Renzo Minelli <renminelli@gmail.com>
2025-08-26 17:17:10 -0300

    add rspec-rails as dependency

--- devise-webauthn.gemspec
+++ devise-webauthn.gemspec
@@ -27,7 +27,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "propshaft", "~> 1.2"
   spec.add_development_dependency "puma", "~> 6.6"
   spec.add_development_dependency "rails", "~> 8.0"
-  spec.add_development_dependency "rspec", "~> 3.13"
+  spec.add_development_dependency "rspec-rails", "~> 8.0"
   spec.add_development_dependency "rubocop", "~> 1.79"
   spec.add_development_dependency "rubocop-rails", "~> 2.32"
   spec.add_development_dependency "rubocop-rspec", "~> 3.6"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,12 +7,17 @@ require_relative "dummy/config/environment"
 ActiveRecord::Migrator.migrations_paths = [File.expand_path("../test/dummy/db/migrate", __dir__)]
 ActiveRecord::Migrator.migrations_paths << File.expand_path("../db/migrate", __dir__)
 
+require "rspec/rails"
+ENV["RAILS_ENV"] ||= "test"
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
+
+  config.use_transactional_fixtures = true
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
