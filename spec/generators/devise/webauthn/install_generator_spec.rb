3c0635f89c08ac7b8f10c240b9fa027aab55ca28
Joaquin Tomas <joaquintomas2003@gmail.com>
2025-08-29 15:41:59 -0300

    Test resource_name is correctly passed down when invoking generators

--- spec/generators/devise/webauthn/install_generator_spec.rb
+++ spec/generators/devise/webauthn/install_generator_spec.rb
@@ -7,36 +7,59 @@ RSpec.describe Devise::Webauthn::InstallGenerator, type: :generator do
   tests described_class
   destination File.expand_path("../../../tmp", __dir__)
 
-  before do
-    prepare_destination
-    add_config_directory
-    add_routes
-    allow(generator).to receive(:invoke)
-    generator.invoke_all
+  context "when using default resource name" do
+    before do
+      prepare_destination
+      add_config_directory
+      add_routes
+      allow(generator).to receive(:invoke)
+      generator.invoke_all
+    end
+
+    it "creates a weabauthn initializer" do
+      expect(File).to exist(File.join(destination_root, "config/initializers/webauthn.rb"))
+    end
+
+    it "mounts the engine in routes.rb" do
+      routes = File.read(File.join(destination_root, "config/routes.rb"))
+      expect(routes).to include('mount Devise::Webauthn::Engine, at: "/devise-webauthn"')
+    end
+
+    it "invokes the passkey model generator" do
+      expect(generator).to have_received(:invoke)
+        .with("devise:webauthn:passkey_model", [], resource_name: "user")
+    end
+
+    it "invokes the webauthn_id column generator" do
+      expect(generator).to have_received(:invoke)
+        .with("devise:webauthn:webauthn_id", [], resource_name: "user")
+    end
+
+    it "invokes the stimulus controller generator" do
+      expect(generator).to have_received(:invoke)
+        .with("devise:webauthn:stimulus")
+    end
   end
 
-  it "creates a weabauthn initializer" do
-    expect(File).to exist(File.join(destination_root, "config/initializers/webauthn.rb"))
-  end
+  context "when using a custom resource name" do
+    before do
+      prepare_destination
+      add_config_directory
+      add_routes
+      generator([destination_root], ["--resource_name=admin"])
+      allow(generator).to receive(:invoke)
+      generator.invoke_all
+    end
 
-  it "mounts the engine in routes.rb" do
-    routes = File.read(File.join(destination_root, "config/routes.rb"))
-    expect(routes).to include('mount Devise::Webauthn::Engine, at: "/devise-webauthn"')
-  end
+    it "invokes the passkey model generator with the custom resource name" do
+      expect(generator).to have_received(:invoke)
+        .with("devise:webauthn:passkey_model", [], resource_name: "admin")
+    end
 
-  it "invokes the passkey model generator" do
-    expect(generator).to have_received(:invoke)
-      .with("devise:webauthn:passkey_model", [], resource_name: "user")
-  end
-
-  it "invokes the webauthn_id column generator" do
-    expect(generator).to have_received(:invoke)
-      .with("devise:webauthn:webauthn_id", [], resource_name: "user")
-  end
-
-  it "invokes the stimulus controller generator" do
-    expect(generator).to have_received(:invoke)
-      .with("devise:webauthn:stimulus")
+    it "invokes the webauthn_id column generator with the custom resource name" do
+      expect(generator).to have_received(:invoke)
+        .with("devise:webauthn:webauthn_id", [], resource_name: "admin")
+    end
   end
 end
 
