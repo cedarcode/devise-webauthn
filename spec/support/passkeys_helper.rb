7470b59efd3b815acf0614d28b2756343a20b140
Joaquin Tomas <joaquintomas2003@gmail.com>
2025-09-01 13:36:25 -0300

    Set `options.resident_key = true` for virtual authenticator

--- spec/support/passkeys_helper.rb
+++ spec/support/passkeys_helper.rb
@@ -5,6 +5,7 @@ module PasskeyHelper
     options = Selenium::WebDriver::VirtualAuthenticatorOptions.new
     options.user_verification = true
     options.user_verified = true
+    options.resident_key = true
     page.driver.browser.add_virtual_authenticator(options)
   end
 end
