50d3240d4cd440e346fc614ebe06f5be2aeb9a86
Santiago Rodriguez <46354312+santiagorodriguez96@users.noreply.github.com>
2025-10-17 16:04:04 -0300

    chore: add generator for controllers

---
+++ lib/generators/devise/webauthn/controllers_generator.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require "rails/generators"
+
+module Devise
+  module Webauthn
+    class ControllersGenerator < Rails::Generators::Base
+      CONTROLLERS = %w[passkeys].freeze
+
+      desc "Create inherited Devise::Webauthn controllers in your app/controllers folder."
+
+      source_root File.expand_path("templates/controllers", __dir__)
+      argument :scope, required: true,
+                       desc: "The scope to create controllers in, e.g. users, admins"
+
+      def create_controllers
+        @scope_prefix = scope.blank? ? "" : "#{scope.camelize}::"
+        CONTROLLERS.each do |name|
+          template "#{name}_controller.rb",
+                   "app/controllers/#{scope}/#{name}_controller.rb"
+        end
+      end
+    end
+  end
+end
