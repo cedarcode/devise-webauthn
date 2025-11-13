b5dd64ae2f94bfec92bd44a8b11067df3b1d3f42
Joaquin Tomas <joaquintomas2003@gmail.com>
2025-11-13 14:46:47 -0300

    --wip-- [skip ci]

--- app/controllers/devise/second_factor_authentication_controller.rb
+++ app/controllers/devise/second_factor_authentication_controller.rb
@@ -2,6 +2,14 @@
 
 module Devise
   class SecondFactorAuthenticationController < DeviseController
-    def new; end
+    def new
+      get_options = WebAuthn::Credential.options_for_get(
+        allow: current_user.webauthn_credentials.pluck(:external_id),
+        user_verification: "discouraged"
+      )
+      session[:current_authentication][:challenge] = get_options.challenge
+
+      @options = get_options
+    end
   end
 end
