b999fa3ae62d0094b3c94ac109a20f5d54df34e5
Joaquin Tomas <joaquintomas2003@gmail.com>
2025-11-13 15:23:20 -0300

    Update credential model allowing to separate passkeys from 2fa keys

--- lib/generators/devise/webauthn/webauthn_credential_model/webauthn_credential_model_generator.rb
+++ lib/generators/devise/webauthn/webauthn_credential_model/webauthn_credential_model_generator.rb
@@ -29,6 +29,10 @@ module Devise
           <<~RUBY.indent(2)
             validates :external_id, :public_key, :name, :sign_count, presence: true
             validates :external_id, uniqueness: true
+
+            enum :authentication_factor, { first_factor: 0, second_factor: 1 }
+
+            scope :passkey, -> { first_factor }
           RUBY
         end
       end
