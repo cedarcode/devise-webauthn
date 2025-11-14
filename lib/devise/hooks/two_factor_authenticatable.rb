564db1a923a6ba21c69a4d564c8eebfe3d529075
Santiago Rodriguez <46354312+santiagorodriguez96@users.noreply.github.com>
2025-11-14 16:58:24 -0300

    Use `auth.raw_session` instead of `auth.env["rack.session"]`

--- lib/devise/hooks/two_factor_authenticatable.rb
+++ lib/devise/hooks/two_factor_authenticatable.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 Warden::Manager.after_authentication do |resource, auth, opts|
-  if resource.second_factor_enabled? && !auth.env["rack.session"][:second_factor_authenticated]
+  if resource.second_factor_enabled? && !auth.raw_session[:second_factor_authenticated]
     auth.logout
-    auth.env["rack.session"][:current_authentication_resource_id] = resource.id
+    auth.raw_session[:current_authentication_resource_id] = resource.id
     throw(:warden, scope: opts[:scope], resource: resource, message: :two_factor_required)
   end
 end
