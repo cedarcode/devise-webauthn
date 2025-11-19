5f0daada3357778e12338a70c1d8b0ba935133b7
Santiago Rodriguez <46354312+santiagorodriguez96@users.noreply.github.com>
2025-11-19 13:36:26 -0300

    test: add request specs

---
+++ spec/requests/devise/second_factor_authentication_controller_spec.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Devise::SecondFactorAuthenticationController, type: :request do
+  let(:user) { User.create!(email: "test@example.com", password: "password123") }
+
+  describe "GET #new" do
+    context "when user is authenticated" do
+      before do
+        sign_in user, scope: :user
+      end
+
+      it "redirects to root path" do
+        get new_user_second_factor_authentication_path
+
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context "when user is not authenticated" do
+      context "when user has not initiated sign in" do
+        it "redirects to the sign-in page" do
+          get new_user_second_factor_authentication_path
+
+          expect(response).to redirect_to(new_user_session_path)
+          expect(flash[:alert]).to eq "Sign in was not initiated."
+        end
+      end
+
+      context "when user has initiated sign in" do
+        before do
+          user.passkeys.create!(
+            external_id: "external-id",
+            name: "My Passkey",
+            public_key: "public-key",
+            sign_count: 0
+          )
+
+          post user_session_path, params: {
+            user: {
+              email: user.email,
+              password: "password123"
+            }
+          }
+        end
+
+        it "renders the new template" do
+          get new_user_second_factor_authentication_path
+
+          expect(response).to have_http_status(:ok)
+        end
+      end
+    end
+  end
+end
---
+++ spec/requests/devise/second_factor_keys_controller_spec.rb
@@ -0,0 +1,124 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "webauthn/fake_client"
+
+RSpec.describe Devise::SecondFactorKeysController, type: :request do
+  let(:user) { User.create!(email: "test@example.com", password: "password123") }
+  let(:client) { WebAuthn::FakeClient.new(WebAuthn.configuration.allowed_origins.first) }
+
+  describe "GET #new" do
+    context "when user is not authenticated" do
+      it "redirects to the sign-in page" do
+        get new_user_second_factor_key_path
+
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+
+    context "when user is authenticated" do
+      before do
+        sign_in user
+      end
+
+      it "renders the new template" do
+        get new_user_second_factor_key_path
+
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+
+  describe "POST #create" do
+    context "when user is not authenticated" do
+      it "redirects to the sign-in page" do
+        post user_second_factor_keys_path
+
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+
+    context "when user is authenticated" do
+      before do
+        sign_in user
+        get new_user_second_factor_key_path # To set the challenge in session
+      end
+
+      context "with valid parameters" do
+        let(:credential) do
+          client.create(
+            challenge: session[:webauthn_challenge],
+            user_verified: false
+          )
+        end
+
+        it "creates a new security key and redirects" do
+          assert_difference("user.second_factor_webauthn_credentials.count", 1) do
+            post user_second_factor_keys_path, params: {
+              public_key_credential: credential.to_json,
+              name: "My Security Key"
+            }
+          end
+
+          expect(response).to redirect_to(new_user_second_factor_key_path)
+          expect(flash[:notice]).to eq "Security Key created successfully."
+          expect(session[:webauthn_challenge]).to be_nil
+        end
+      end
+
+      context "with invalid credential" do
+        let(:invalid_credential) do
+          client.create(
+            challenge: session[:webauthn_challenge],
+            user_present: false
+          )
+        end
+
+        it "does not create a new security key and redirects" do
+          assert_difference("user.second_factor_webauthn_credentials.count", 0) do
+            post user_second_factor_keys_path, params: {
+              public_key_credential: invalid_credential.to_json,
+              name: "My Security Key"
+            }
+          end
+
+          expect(response).to redirect_to(new_user_second_factor_key_path)
+          expect(flash[:alert]).to eq "Webauthn credential verification failed."
+          expect(session[:webauthn_challenge]).to be_nil
+        end
+      end
+    end
+  end
+
+  describe "DELETE #destroy" do
+    context "when user is not authenticated" do
+      it "redirects to the sign-in page" do
+        delete user_second_factor_key_path(1)
+
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+
+    context "when user is authenticated" do
+      let!(:security_key) do
+        user.second_factor_webauthn_credentials.create!(
+          external_id: "external-id",
+          name: "My Passkey",
+          public_key: "public-key",
+          sign_count: 0
+        )
+      end
+
+      before do
+        sign_in user, scope: :user
+      end
+
+      it "deletes the security key and redirects" do
+        assert_difference("user.second_factor_webauthn_credentials.count", -1) do
+          delete user_second_factor_key_path(security_key)
+          expect(response).to redirect_to(new_user_second_factor_key_path)
+        end
+      end
+    end
+  end
+end
