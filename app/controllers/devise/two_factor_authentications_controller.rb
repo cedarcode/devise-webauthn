8326241a248d59e4f01718cf68d4e3a73d4e4a2a
Santiago Rodriguez <46354312+santiagorodriguez96@users.noreply.github.com>
2025-12-03 13:34:18 -0300

    fix: set translations scope to be `devise.sessions` for 2fa auth

--- app/controllers/devise/two_factor_authentications_controller.rb
+++ app/controllers/devise/two_factor_authentications_controller.rb
@@ -16,7 +16,7 @@ module Devise
 
     def create
       self.resource = warden.authenticate!(auth_options)
-      set_flash_message! :notice, :signed_in
+      set_flash_message! :notice, :signed_in, scope: :"devise.sessions"
       sign_in(resource_name, resource)
       yield resource if block_given?
       respond_with resource, location: after_sign_in_path_for(resource)
