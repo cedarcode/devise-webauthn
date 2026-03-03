8c4f5f738cc2167e9346c574cf89017a02008076
Renzo Minelli <renminelli@gmail.com>
2026-03-03 11:30:47 -0300

    fix rubocop offenses

--- lib/devise/webauthn/helpers/credentials_helper.rb
+++ lib/devise/webauthn/helpers/credentials_helper.rb
@@ -5,7 +5,7 @@ module Devise
     module CredentialsHelper
       def passkey_creation_form_for(resource_or_resource_name, **options, &block)
         form_with(
-          **options.merge(url: passkeys_path(resource_or_resource_name), method: :post)
+          **options, url: passkeys_path(resource_or_resource_name), method: :post
         ) do |f|
           tag.webauthn_create(data: { options_url: passkey_registration_options_path(resource_or_resource_name) }) do
             concat f.hidden_field(:public_key_credential, data: { webauthn_target: "response" })
@@ -16,7 +16,7 @@ module Devise
 
       def login_with_passkey_form_for(resource_or_resource_name, **options, &block)
         form_with(
-          **options.merge(url: session_path(resource_or_resource_name), method: :post)
+          **options, url: session_path(resource_or_resource_name), method: :post
         ) do |f|
           tag.webauthn_get(data: { options_url: passkey_authentication_options_path(resource_or_resource_name) }) do
             concat f.hidden_field(:public_key_credential, data: { webauthn_target: "response" })
@@ -27,7 +27,7 @@ module Devise
 
       def security_key_creation_form_for(resource_or_resource_name, **options, &block)
         form_with(
-          **options.merge(url: second_factor_webauthn_credentials_path(resource_or_resource_name), method: :post)
+          **options, url: second_factor_webauthn_credentials_path(resource_or_resource_name), method: :post
         ) do |f|
           tag.webauthn_create(
             data: { options_url: security_key_registration_options_path(resource_or_resource_name) }
@@ -40,7 +40,7 @@ module Devise
 
       def login_with_security_key_form_for(resource_or_resource_name, **options, &block)
         form_with(
-          **options.merge(url: two_factor_authentication_path(resource_or_resource_name), method: :post)
+          **options, url: two_factor_authentication_path(resource_or_resource_name), method: :post
         ) do |f|
           tag.webauthn_get(data: {
                              options_url: security_key_authentication_options_path(resource_or_resource_name)
